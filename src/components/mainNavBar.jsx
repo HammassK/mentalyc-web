b776c31bfb7e541376a318bb7e1cbabfb3ae2749
HammassK <hammas.khan@agiledistrict.com>
2024-11-29 14:25:50 +0500

    feat: nav bar developed

--- src/components/mainNavBar.jsx
+++ src/components/mainNavBar.jsx
@@ -1,5 +1,115 @@
+import {
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
+import { navbarStyles } from "../styles/layoutStyles";
+import AppLogo from "../assets/svgs/navbar/appLogo";
+import ProIcon from "../assets/svgs/navbar/proIcon";
+import { NavLinks } from "../utils/constants";
+import EarnIcon from "../assets/svgs/navbar/earnIcon";
+import SuperButton from "../assets/svgs/navbar/superButton";
+import { useState } from "react";
+import ArrowDown from "../assets/svgs/navbar/arrowDown";
+import NotesIcon from "../assets/svgs/navbar/notesIcon";
+import InfoIcon from "../assets/svgs/navbar/infoIcon";
+
 const MainNavBar = () => {
-  return <div>MainNavBar</div>;
+  const {
+    navbarContainer,
+    navbarSubContainer,
+    appLogoContainer,
+    navLinksContainer,
+    navLinkText,
+    notesContainer,
+    notesText,
+    navAvatar,
+    avatarText,
+  } = navbarStyles;
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleAvatarClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Box sx={navbarContainer}>
+      <Box sx={navbarSubContainer}>
+        <Box sx={appLogoContainer}>
+          <AppLogo />
+          <ProIcon />
+        </Box>
+        <Box sx={navLinksContainer}>
+          {NavLinks.map((item, index) => (
+            <Typography
+              key={index}
+              sx={{
+                ...navLinkText,
+                color: item?.name === "Clients" ? "#731054" : "#707070",
+                fontWeight: item?.name === "Clients" ? "700" : "400",
+                borderBottom:
+                  item?.name === "Clients"
+                    ? "4px solid #731054"
+                    : "4px solid #ffffff",
+              }}
+            >
+              {item?.name}
+            </Typography>
+          ))}
+
+          <EarnIcon />
+        </Box>
+        <Box sx={notesContainer}>
+          <NotesIcon />
+          <Typography sx={notesText}>12 notes left</Typography>
+          <InfoIcon />
+          <SuperButton />
+          <IconButton
+            onClick={handleAvatarClick}
+            sx={{
+              "&:focus": {
+                outline: "none",
+                boxShadow: "none",
+              },
+              "&:hover": {
+                backgroundColor: "transparent",
+              },
+            }}
+          >
+            <Avatar sx={navAvatar}>
+              <Typography sx={avatarText}>M</Typography>
+            </Avatar>
+            <ArrowDown />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleMenuClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Settings</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+          </Menu>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default MainNavBar;
--- src/styles/layoutStyles.jsx
+++ src/styles/layoutStyles.jsx
@@ -15,8 +15,71 @@ const layoutStyles = {
     flexDirection: "column",
     width: "100vw",
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#f0f0f0",
   },
 };
 
-export { layoutStyles };
+const navbarStyles = {
+  navbarContainer: {
+    height: "65px",
+    backgroundColor: "white",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
+  navbarSubContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    height: "100%",
+    width: "85%",
+    maxWidth: "1440px",
+  },
+  appLogoContainer: {
+    width: "90px",
+    display: "flex",
+    height: "45%",
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+  },
+  navLinksContainer: {
+    display: "flex",
+    alignItems: "center",
+    gap: "44px",
+    height: "100%",
+  },
+  navLinkText: {
+    display: "flex",
+    alignItems: "start",
+    height: "55%",
+    alignSelf: "end",
+  },
+  notesContainer: {
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+  },
+  notesText: {
+    fontSize: "16px",
+    fontWeight: "400",
+    color: "#707070",
+  },
+  navAvatar: {
+    backgroundColor: "#D6E4F4",
+    width: "44px",
+    height: "44px",
+    marginRight: "10px",
+    color: "#040404",
+  },
+  avatarText: {
+    fontWeight: 400,
+    borderRadius: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
+
+export { layoutStyles, navbarStyles };
